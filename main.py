84ce61bfdf37b9425ee7ebd99a13bf240e0f1062
Vladislav Bystritskii <dev@bystritskii.com>
2024-06-01 13:51:34 +0400

    minor fixes

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from Helpers.UnarchiveService import UnarchiveService
+from Helpers.ArchiveService import ArchiveService
 import os
 
 if __name__ == "__main__":
     archive_path = input("Enter the path to the archive: ").replace("\\ ", " ").strip()
     extract_to = input("Enter the path to extract to: ").replace("\\ ", " ").strip()
 
-    # Normalize paths to handle any escape characters and spaces
     archive_path = os.path.normpath(archive_path)
     extract_to = os.path.normpath(extract_to)
 
-    service = UnarchiveService()
+    service = ArchiveService()
     try:
         service.extract(archive_path, extract_to)
     except ValueError as e:
